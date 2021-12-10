f1397d2154616c2eebd7075e5f61157f9775cffb
毛 <1296193245@qq.com>
2021-12-10 20:37:31 +0800

    java_course_design_Swing_implements_text_editor

---
+++ src/main/java/UI/Function.java
@@ -0,0 +1,19 @@
+package UI;
+
+/**
+ * Project name(项目名称)：java课程设计 Swing实现文本编辑器
+ * Package(包名): UI
+ * Interface(接口名): Function
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2021/12/10
+ * Time(创建时间)： 20:37
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public interface Function
+{
+
+}
